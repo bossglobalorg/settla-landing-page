8852f7f5b4d0958d7c44c71b44ee945b4cd8ac12
AJ1732 <ejemeniboi@gmail.com>
2024-11-15 21:12:29 +0100

    chore: build desktop navigation

--- src/components/layout/components/Navigation/index.tsx
+++ src/components/layout/components/Navigation/index.tsx
@@ -1,23 +1,56 @@
-import ButtonLink from "@/components/ui/ButtonLink";
+import Link from "next/link";
+import { ButtonLink } from "@/components";
+import { SettlaLogoSVG, SettlaWordMarkSVG } from "@/components/svgs";
 
 const Navigation = () => {
   return (
     <header className="full-width content-grid fixed min-h-[5.125rem] w-full bg-white/30 backdrop-blur-md">
       <nav className="flex items-center justify-between">
-        <div className="h-[1.75rem] w-[8.37513rem] rounded bg-green-500" />
+        <div className="flex items-center justify-start gap-4 rounded">
+          <SettlaLogoSVG />
+          <SettlaWordMarkSVG />
+        </div>
 
         <ul className="flex items-center justify-between gap-10">
-          {[1, 2, 3, 4].map((item) => (
-            <li key={item}>link {item}</li>
+          {navlinks.map(({ label, link }) => (
+            <li key={label} className="text-[0.9375rem] capitalize text-white">
+              <Link href={link}>{label}</Link>
+            </li>
           ))}
         </ul>
 
         <div className="space-x-3">
-          <ButtonLink variant="primary-outline">Login</ButtonLink>
-          <ButtonLink>Get Started</ButtonLink>
+          <ButtonLink
+            variant="primary-outline"
+            className="px-5 py-2.5 text-[0.9375rem]"
+          >
+            Log in
+          </ButtonLink>
+          <ButtonLink className="px-5 py-2.5 text-[0.9375rem]">
+            Get Started
+          </ButtonLink>
         </div>
       </nav>
     </header>
   );
 };
 export default Navigation;
+
+const navlinks = [
+  {
+    label: "case studies",
+    link: "",
+  },
+  {
+    label: "about us",
+    link: "",
+  },
+  {
+    label: "help center",
+    link: "",
+  },
+  {
+    label: "contact us",
+    link: "",
+  },
+];
--- src/components/svgs/index.tsx
+++ src/components/svgs/index.tsx
@@ -1,1 +1,3 @@
 export { default as ArrowRightSVG } from "./ArrowRightSVG";
+export { default as SettlaLogoSVG } from "./SettlaLogoSVG";
+export { default as SettlaWordMarkSVG } from "./SettlaWordMarkSVG";
