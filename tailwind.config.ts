a676c239ad6d683fd07ed493cb86e930b81eeef3
AJ1732 <ejemeniboi@gmail.com>
2024-11-15 19:26:11 +0100

    style: add custom colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,25 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        accent: {
+          400: "#FEDB75",
+        },
+        primary: {
+          400: "#50B8AA",
+          600: "#059A86",
+          900: "#00332C",
+          1000: "#002C15",
+        },
+        gray: {
+          50: "#F2F5F7",
+          100: "#EDEFEB",
+          200: "#E5EBE5",
+          300: "#C9CBCE",
+          400: "#A7A9AB",
+          500: "#6A6C6A",
+          600: "#454745",
+          900: "#0E0F0C",
+        },
       },
     },
   },
