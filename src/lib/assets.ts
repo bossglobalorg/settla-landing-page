f3d85f3476fafa5540028df4294799efec5cd727
AJ1732 <ejemeniboi@gmail.com>
2024-11-16 20:36:35 +0100

    chore: add logos to featured section

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -1,6 +1,13 @@
 const assetLib = {
   // IMAGES
   homePageHeroImage: "/imgs/settla-homepage-hero-image.png",
+
+  // SVGS
+  techcabalLogo: "/svgs/techcabal-logo.svg",
+  techpointLogo: "/svgs/techpoint-logo.svg",
+  techcrunchLogo: "/svgs/techcrunch-logo.svg",
+  forbesLogo: "/svgs/forbes-logo.svg",
+  techradarLogo: "/svgs/techradar-logo.svg",
 };
 
 export default assetLib;
