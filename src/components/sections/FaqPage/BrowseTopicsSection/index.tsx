e80f1e86f8c0bfe8fa7237206b1aa742af19fcde
AJ1732 <ejemeniboi@gmail.com>
2024-12-02 21:07:13 +0100

    chore: update filter functionality

--- src/components/sections/FaqPage/BrowseTopicsSection/index.tsx
+++ src/components/sections/FaqPage/BrowseTopicsSection/index.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Image from "next/image";
 import { topics } from "./constants";
+import { useValueContext } from "@/provider";
+import { cn } from "@/lib/utils";
 
 const BrowseTopicsSection = () => {
+  const { faqState, faqDispatch } = useValueContext();
+
   return (
     <section className="content-grid overflow-hidden">
       <div className="mx-auto w-full max-w-[63rem] space-y-8">
@@ -9,28 +14,51 @@ const BrowseTopicsSection = () => {
           Browse topics
         </h2>
 
-        <div className="md:no-scrollbar flex grid-cols-1 place-items-center gap-8 overflow-auto max-md:mb-4 md:grid md:grid-cols-2 lg:grid-cols-3">
-          {topics.map(({ icon, title, items }, index) => (
-            <article
-              key={`topic ${index + 1}`}
-              className="size-full min-w-80 max-w-80 rounded-2xl bg-gray-50 px-6 py-8"
-            >
-              <figure className="relative mb-6 size-12 rounded-full bg-accent-400">
-                <Image src={icon} alt={`topic ${index + 1} icon`} fill />
-              </figure>
+        <div className="md:no-scrollbar flex grid-cols-1 place-items-center gap-8 max-lg:overflow-auto max-md:mb-4 md:grid md:grid-cols-2 lg:grid-cols-3">
+          {topics.map(({ icon, title, items }, index) => {
+            return (
+              <article
+                key={`topic ${index + 1}`}
+                onClick={() =>
+                  faqDispatch({ type: "SET_FILTER", payload: title })
+                }
+                className={cn(
+                  "size-full min-w-80 max-w-80 cursor-pointer rounded-2xl bg-gray-50 px-6 py-8",
+                  faqState.selectedFilter === title
+                    ? "border-primary-900 bg-primary-900 text-white"
+                    : "border-gray-200 bg-gray-50 hover:bg-gray-200",
+                )}
+              >
+                <figure className="relative mb-6 size-12 rounded-full bg-accent-400">
+                  <Image src={icon} alt={`topic ${index + 1} icon`} fill />
+                </figure>
 
-              <h3 className="mb-3 text-lg font-medium leading-7 text-primary-900">
-                {title}
-              </h3>
+                <h3
+                  className={cn(
+                    "mb-3 text-lg font-medium leading-7 first-letter:capitalize",
+                    faqState.selectedFilter === title
+                      ? "text-accent-400"
+                      : "text-primary-900",
+                  )}
+                >
+                  {title}
+                </h3>
 
-              <ul className="space-y-2 leading-6 text-gray-600">
-                {items.map((item, idx) => (
-                  <li key={idx}>{item}</li>
-                ))}
-              </ul>
-              <p></p>
-            </article>
-          ))}
+                <ul
+                  className={cn(
+                    "space-y-2 leading-6",
+                    faqState.selectedFilter === title
+                      ? "text-gray-200"
+                      : "text-gray-600",
+                  )}
+                >
+                  {items.map((item, idx) => (
+                    <li key={idx}>{item}</li>
+                  ))}
+                </ul>
+              </article>
+            );
+          })}
         </div>
       </div>
     </section>
--- src/components/sections/FaqPage/SearchSection/components/FilterTabs/index.tsx
+++ src/components/sections/FaqPage/SearchSection/components/FilterTabs/index.tsx
@@ -6,26 +6,22 @@ const FilterTabs = () => {
   const { faqState, faqDispatch } = useValueContext();
 
   const filters = [
-    "sending money",
-    "managing your account",
-    "receiving money",
-    "settla support",
-    "conversion",
     "pay-in",
     "account",
+    "pay-outs",
     "security",
     "track a payment",
   ];
 
   return (
     <div className="relative mx-auto w-full">
-      <div className="no-scrollbar flex items-center gap-4 overflow-x-auto">
+      <div className="no-scrollbar flex items-center gap-4 overflow-x-auto lg:justify-center">
         {filters.map((filter: string) => (
           <button
             key={filter}
             onClick={() => faqDispatch({ type: "SET_FILTER", payload: filter })}
             className={cn(
-              "flex-shrink-0 whitespace-nowrap rounded-full border px-4 py-2.5 text-sm font-semibold capitalize leading-6 text-primary-900 transition-all duration-200 sm:text-base",
+              "flex-shrink-0 whitespace-nowrap rounded-full border px-4 py-2.5 text-sm font-semibold leading-6 text-primary-900 transition-all duration-200 first-letter:capitalize sm:text-base",
               faqState.selectedFilter === filter
                 ? "border-primary-900 bg-primary-900 text-white"
                 : "border-gray-200 bg-gray-50 hover:bg-gray-200",
